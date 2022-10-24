513e2c9d019d79d4b7e61fab106220e95d843617
Joshua Black <joshuablack.ecommerce@gmail.com>
2022-10-24 16:42:55 -0500

    Revert "Revert "Client server pilot""

--- qswitch/src/main.rs
+++ qswitch/src/main.rs
@@ -1,3 +1,17 @@
+use qserver::UdpServiceListener;
+use tokio::{net::UdpSocket, runtime};
  fn main(){
     println!("Hello world");
+    let rt = runtime::Builder::new_multi_thread().enable_all().build().unwrap();
+    let s1 = UdpServiceListener::start("127.0.0.1:0", &rt);
+    let s2 = UdpServiceListener::start("127.0.0.1:0", &rt);
+    let s3 = UdpServiceListener::start("127.0.0.1:0", &rt);
+    s2.initiate_udp_link(s1.get_local_addr().unwrap());
+    s3.initiate_udp_link(s1.get_local_addr().unwrap());
+    let l2 = s2.get_new_link().unwrap();
+    let l3 = s3.get_new_link().unwrap();
+    l2.tx().send((5,[1;500])).unwrap();
+    l3.tx().send((5,[1;500])).unwrap();
+    s1.stop(&rt);
+    loop{}
  }
