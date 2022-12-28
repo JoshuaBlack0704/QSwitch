29ec22189c9a47cd44b1393c4f887268181d0c31
Joshua Black <joshuablack.ecommerce@gmail.com>
2022-12-28 01:41:26 -0600

    Cleanup

--- qsim/src/main.rs
+++
@@ -1,8 +0,0 @@
-use std::net::ToSocketAddrs;
-
-use quniverse::Universe;
-
-fn main() {
-    let addr = "127.0.0.1:0".to_socket_addrs().unwrap().last().unwrap();
-    let universe = Universe::load(addr, None);
-}
